e9b825ed90f2e00802372b67a255da55daf2b95d
Ronald Konjer <ronaldkonjer@gmail.com>
2018-03-07 17:37:54 +0100

    Revert "Merge branch 'recipes/splunk' into develop"
    This reverts commit 4647708c8dbbefba5cb2cbb5a2253551d859ee17, reversing
    changes made to 7cedab4c7fe2caa60f02f1fb8f5184bf7dab2735.

--- recipes/splunk/setup-cluster.sh
+++
@@ -1,5 +0,0 @@
-# Create the forwarder service account.
-# Add priviledges to mount volumes and run as root.
-oc create sa splunk-forwarder
-oadm policy add-scc-to-user anyuid system:serviceaccount:default:splunk-forwarder
-oadm policy add-scc-to-user privileged system:serviceaccount:default:splunk-forwarder
