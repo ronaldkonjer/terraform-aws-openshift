b040114dbd8a32fb846faff8cccfd0858ce4c7e2
Ronald Konjer <ronaldkonjer@gmail.com>
2018-03-07 15:53:27 +0100

    own environment adjustments

--- modules/openshift/03-security-groups.tf
+++ modules/openshift/03-security-groups.tf
@@ -64,6 +64,13 @@ resource "aws_security_group" "openshift-public-ingress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name    = "OpenShift Public Access"
     Project = "openshift"
@@ -90,6 +97,13 @@ resource "aws_security_group" "openshift-public-egress" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
